71c8c1438c190023a3a2739cc33006d291feba5f
LindenDaniels <lindendaniels@gmail.com>
2015-01-13 04:04:30 -0500

    Update daniels_linden_assignment2.js
    Added in placeholders for strings; examples of Boolean Operators.

--- daniels_linden_assignment2.js
+++ daniels_linden_assignment2.js
@@ -1,3 +1,5 @@
+//AND (p && q), OR (p || q), NOT (!p), XOR (Either or but not both.) (p || q && !(p && q)
+
 //alert("JavaScript works!"); I'm code that's been commented out.
 //console.log("I would be a string with code, but I'm commented out too!) //Add comments to code like this!
 // Below is an example of a block quote. It looks fancier than using multiple lines of //.
@@ -6,3 +8,9 @@ Linden Daniels
 SDI1501
 Project 2
 */
+
+var myString = "I'm a string.",
+    myNum = 7,
+    my Boolean = true;
+    
+ ((p && q))
