513ceafd83bc1dfb4b76aad6a9e3d1dc358d8d69
LindenDaniels <lindendaniels@gmail.com>
2015-01-13 03:30:11 -0500

    Update daniels_linden_assignment2.js
    Commented out alert code and added in examples of comments.

--- daniels_linden_assignment2.js
+++ daniels_linden_assignment2.js
@@ -1,1 +1,8 @@
-alert("JavaScript works!");
+//alert("JavaScript works!"); I'm code that's been commented out.
+//console.log("I would be a string with code, but I'm commented out too!) //Add comments to code like this!
+// Below is an example of a block quote. It looks fancier than using multiple lines of //.
+/*
+Linden Daniels
+SDI1501
+Project 2
+*/
