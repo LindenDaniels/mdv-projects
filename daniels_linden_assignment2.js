6d43d7c17f0a7548d8a7c32755e94576c4fc5c41
LindenDaniels <lrdaniels@fullsail.edu>
2015-01-16 20:07:31 -0500

    Added for loop, numnber conditional.

--- daniels_linden_assignment2.js
+++ daniels_linden_assignment2.js
@@ -73,6 +73,37 @@ while (sex != "male" && sex != "female")
 
 
  chooseSex();
+ 
+ if (sex === "male")
+ 	{
+ 	var gender = "He";
+ 	}
+ 	else {
+ 		var gender = "She";
+ 		}
+ 
+ var myName = prompt ("What would you like to name your new puppy?")
+ 	console.log ("Your new puppy's name is " + myName + ".")
+ 	
+ 	console.log (myName + " barks happily at you and twirls around in a circle. " + gender +
+ 	" then peers up at you expectantly with large puppy dog eyes.")
+ 	
+ 	var feedMe = confirm (myName + " is hungry! Would you like to feed " + myName + "?")
+
+ 
+ if (feedMe === true) 
+ 	{ for (foodCounter = 5; foodCounter >= 1; foodCounter--)
+ {
+ 	var feedMe = confirm (myName + " happily gobbles down the bowl of food. Would you like to feed " 
+ 			+ myName + " again?") }
+ 			
+ 			console.log ("Uh oh! You have run out of food.");
+ 			
+ } else {
+ 			console.log ("You have chosen not to feed " + myName + ".") 
+ 		}
+ 
+ 
 
 
 
