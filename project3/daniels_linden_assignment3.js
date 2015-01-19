6dab917d4a8bc12691360f0f73acae78ce682845
LindenDaniels <lrdaniels@fullsail.edu>
2015-01-19 17:19:21 -0500

    Added array, math.

---
+++ project3/daniels_linden_assignment3.js
@@ -0,0 +1,19 @@
+var pupWeights = ["15, 5, 6, 10, 8, 20"];
+
+for (var i = 0; i < pupWeights.length; i++) {
+
+var myMean = (15 * 5 * 6 * 10 * 8 * 20) /i
+
+console.log(myMean);
+
+
+console.log(i);
+
+//var mySum = i
+//var myMean = mySum / pupWeights.length
+
+//console.log(myMean);
+
+
+
+};
